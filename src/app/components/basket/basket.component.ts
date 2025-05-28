fa86319ffb7dfbc9f4083f98b273869749b0b864
Thalia Mijas <tlmijas@outlook.com>
2025-05-28 15:21:48 -0500

    Agregar formulario de envio

--- src/app/components/basket/basket.component.ts
+++ src/app/components/basket/basket.component.ts
@@ -1,12 +1,51 @@
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { mockProducts } from '../../mocks/cart-mock';
 
 @Component({
   selector: 'app-basket',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CurrencyPipe, ReactiveFormsModule],
   templateUrl: './basket.component.html',
-  styleUrl: './basket.component.css'
+  styleUrl: './basket.component.css',
 })
 export class BasketComponent {
+  products = mockProducts;
+  shipment: FormGroup;
 
+  get totalPrice() {
+    return this.products.reduce(
+      (sum, product) => sum + product.price * product.quantity,
+      0
+    );
+  }
+
+  constructor(private shipmentBuilder: FormBuilder) {
+    this.shipment = this.shipmentBuilder.group({
+      nombres: ['', [Validators.required]],
+      apellidos: ['', [Validators.required]],
+      direccion: ['', [Validators.required, Validators.email]],
+      cp: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(10),
+        ],
+      ],
+      telefono: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    const shipmentData = this.shipment.value;
+    console.log(shipmentData);
+    this.shipment.reset();
+  }
 }
---
+++ src/app/mocks/cart-mock.ts
@@ -0,0 +1,31 @@
+export interface Product {
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+  quantity: number;
+}
+
+export const mockProducts: Product[] = [
+  {
+    id: 1,
+    title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    price: 109.95,
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: 'Mens Casual Slim Fit',
+    image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    price: 15.99,
+    quantity: 2,
+  },
+  {
+    id: 8,
+    title: 'Pierced Owl Rose Gold Plated Stainless Steel Double',
+    image: 'https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg',
+    price: 10.99,
+    quantity: 3,
+  },
+];
